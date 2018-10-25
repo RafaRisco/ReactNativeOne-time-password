3d95c36cfbe506f9d49d064dedcf2cbc82eb497d
RafaRisco <30323092+RafaRisco@users.noreply.github.com>
2018-10-24 21:02:18 -0500

    Delete twilio.js

--- functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio');
-
-const accountSid = 'AC06f9c3f52ee4483b3b9d041d21292453';
-const authToken = '220fd3e5e411189913a5fa4affa7e723';
-
-module.export = new twilio.Twilio(accountSid, authToken);
